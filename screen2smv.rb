107473e625d80c6f710cc465ca7355fea813ac5d
Twan Coenraad <t.coenraad@student.utwente.nl>
2016-05-04 09:20:37 +0200

    Make screen2smv an executable

--- screen2smv.rb
+++ screen2smv.rb
@@ -1,5 +1,11 @@
+#!/usr/bin/env ruby
 require_relative 'board'
 
-File.open('screen.2000', 'r') do |f|
+if ARGV.empty?
+  puts 'usage: screen2smv.rb SCREEN_FILE'
+  exit
+end
+
+File.open(ARGV.first, 'r') do |f|
   puts Board.parse(f.read).to_smv
 end
